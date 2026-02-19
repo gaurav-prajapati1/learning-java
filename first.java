70cc7576e919a5261f3d7adf4f6b8d02163f74d9
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-02-19 10:48:01 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,6 @@
+public class first {
+    public static void main(String[] args) {
+        // This prints a friendly message to the console
+        System.out.println("Hello! Your Java environment is working.");
+    }
+}
